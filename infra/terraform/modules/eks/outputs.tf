8485d6595d3a2a779a23acf3e90569d33fe53786
ivanzao <ivan.assalim@gmail.com>
2026-03-24 18:17:22 -0300

    Adjust HPA config

--- infra/terraform/modules/eks/outputs.tf
+++ infra/terraform/modules/eks/outputs.tf
@@ -12,3 +12,4 @@ output "cluster_security_group_id" {
   description = "Security group ID attached to the EKS cluster"
   value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
 }
+
